4d8953d9eb1166115ad531218720179cc4a174ac
Esther Falayi <estherfalayi@Esther-Falayi.local>
2018-03-09 17:57:21 +0100

    - Complete landing page modals

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,30 @@
 $(document).ready(() => {
+  // Add show-modal class
+  function showModal(modalDiv) {
+    const modal = document.querySelector(modalDiv);
+    modal.classList.toggle("show-modal");
+  }
+
+  // Remove show-modal class
   function toggleModal(event) {
     $(".modal").removeClass("show-modal");
   }
 
+  // Display categories modal
   function showCategories(event) {
-    const modal = document.querySelector("#choose-category");
-    modal.classList.toggle("show-modal");
+    showModal("#choose-category");
   }
 
+  // Display getListedForm modal
   function showGetListedForm(event) {
-    const modal = document.querySelector("#get-listed-form");
-    modal.classList.toggle("show-modal");
+    showModal("#get-listed-form");
   }
 
   const getListed = document.querySelector('button#get-listed');
   const closeModal = document.querySelectorAll(".close-modal");
   const categories = document.querySelectorAll(".category");
 
+  // Event listeners
   getListed.addEventListener('click', showCategories);
   closeModal.forEach(closebutton => closebutton.addEventListener('click', toggleModal));
   categories.forEach(category => category.addEventListener('click', showGetListedForm));
